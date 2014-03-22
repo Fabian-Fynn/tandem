68eebd8da94489c4e3eaf216a642c8fd2d91637f
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2014-03-22 16:41:27 +0100

    mmp1 kick off :)

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Widgets</title>
+        <meta name="description" content="An interactive getting started guide for Edge Code CC.">
+        <link rel="stylesheet" href="style/main.css">
+        <script src="http://use.edgefonts.net/source-code-pro:n9,n7,n2,n3,n4,n6,n5:all;source-sans-pro:n4,n9,n7,i7,i4,n3,i3,n6,i6,i9,n2,i2:all.js"></script>
+    </head>
+    <body>
+		<header>Widgets</header>
+		<form method="post" action="upload.php"enctype="multipart/form-data">
+			
+			<textarea name="newWidget" class="newWidget" accept="" placeholder="enter your code here" type="textbox"></textarea>
+			
+			<p><input type="submit" value="Senden">
+		</form>
+	</body>
+</html>
