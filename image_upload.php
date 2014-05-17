04a276677a4438b20094d28704f8b704fa4756a0
Fabian Johannes Hoffmann <fhoffmann.mmtb-2013@fh-salzburg.ac.at>
2014-05-17 19:53:28 +0200

    exection handling pictureupload

--- image_upload.php
+++ image_upload.php
@@ -4,32 +4,32 @@
 	{
 		header('Location: index.php');
 	}
+?>
+
+<?php
+  try{
 	$id = $_SESSION['id'];
 	if ($_SERVER['REQUEST_METHOD'] == 'POST')
   {
-    try{
+    
       $uploaddir = dirname( $_SERVER["SCRIPT_FILENAME"] ) . "/img/";
    
       if(!isset($_FILES['image']))
-        throw new Exception("filesize",1);
+        throw new Exception("filesize");
 
         
       $filename = basename($_FILES['image']['name']);
       $ext = substr($filename, -4);
        
 
-    }
-    catch(Exception $e)
-    {
-      die("Das Bild ist zu groß. Bitte wähle ein anderes.");
-      exit;
-    }
+    
+    
     
     $imgname = "temp_".$id.$ext;
-    if( $ext != '.jpg' && $ext != '.png' && $ext != '.JPG' && $ext != '.PNG' ) {
-       die("ich darf nur jpg-Dateien hochladen, nicht " . substr($filename, -3) );
-      exit;
-    }
+    if( $ext != '.jpg' && $ext != '.png' && $ext != '.JPG' && $ext != '.PNG' )
+       throw new Exception("filetype");
+      //"ich darf nur jpg-Dateien hochladen, nicht " . substr($filename, -3) 
+    
      
     $uploadfile = $uploaddir . $imgname;
      
@@ -39,11 +39,35 @@
       resize("img/".$imgname, "img/".$imgname, 500, 500, false);
       header("Location:crop.php");
     } else {
-      echo "Problem beim Speichern der Datei.\n";
+      throw new Exception("saveError");
+      //echo "Problem beim Speichern der Datei.\n";
     }
-     
+
     
   }
+  }
+    catch(Exception $e)
+    {
+      $errorType = $e->getmessage();
+      switch ($errorType) {
+        case 'filesize':
+          $error = 'Das Bild ist zu groß, bitte wähle ein kleineres.';
+          break;
+        case 'filetype':
+          $error = 'Der Dateityp des gewählten Bildes wird nicht unterstützt. Bitte wähle ein png oder jpg.';
+          break;
+        case 'saveError':
+          $error = 'Es ist ein Fehler beim Speichern des Bildes aufgetreten. Bitte versuche es erneut.';
+          break;
+            
+        default:
+          $error = 'Es ist ein unerwarteter Fehler aufgetreten. Das tut mir leid. Bitte versuche es erneut.';
+          break;
+      }
+      
+    }
+    finally
+    {
 ?>
 <script type="text/javascript" src="js/mootools.js"></script>
 <script type="text/javascript" src="js/Lasso.js"></script>
@@ -52,7 +76,12 @@
 <div class = "wrap">
 		
 	<section class="profileTop">
-		<div class="userName"><h1>Profilbild hochladen</h1></div>
+		
+  <?php
+
+    if(!isset($e)):
+  ?>
+    <div class="userName"><h1>Profilbild hochladen</h1></div>
 		<article class="left">
 			
     <form method="post" action="image_upload.php" id="picForm" enctype="multipart/form-data">
@@ -67,7 +96,7 @@
  $('#fileToUpload').bind('change', function() {
 
   //this.files[0].size gets the size of your file.
-  if(this.files[0].size > 1000000)
+  if(this.files[0].size > 8000000)
     {
       $('#submit').attr("disabled", "disabled");
       $('#error').html("Das Bild ist leider zu groß");
@@ -81,12 +110,22 @@
 });  </script>
 <?php
   
-  if($_SERVER['REQUEST_METHOD'] == 'POST')
-    { ?>
+  if($_SERVER['REQUEST_METHOD'] == 'POST'):
+   ?>
     <img src="img/temp_<?php echo ($id.$ext); ?>"  id="cropbox"/>
 
 <?php
-  }
+  endif;
+  else:
+    ?>
+  
+  <article class="left">
+    <?php echo($error); ?>
+  </article>
+
+  <?php
+  endif;
+
   	?>
     </article>
 		<article class="right">
@@ -96,5 +135,6 @@
 </div>
 <?php
     include "footer.php";
+  }
 ?>
 
