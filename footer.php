ccace0503be0402548853411b20ab22b412024db
Fabian Johannes Hoffmann <fhoffmann.mmtb-2013@fh-salzburg.ac.at>
2014-05-21 15:46:36 +0200

    footer design

--- footer.php
+++ footer.php
@@ -3,18 +3,26 @@
   	<div class="left">
 		<a href="mailto:fhoffmann.mmt-b2013@fh-salzburg.at">Fabian Hoffmann</a><br>
   		<a href="impressum.php">Impressum</a>
-  
-  </div>
-  <div class="right"><img src="img/mtfbwy.png" alt="mtfbwy"><?php 
+  <?php 
 
   if($_SESSION['id'] == "3")
   {
   	?>
+  	<br>
 		<a href="createusers.php">create User</a>
+		<br>
 		<a href="addcourse.php">edit courses</a>
   	<?php
   }
 ?>
+  </div>
+  <div class="right">
+	<div class="logofooter">
+	<img src="img/icons/cap.png" alt="logo">
+</div>
+
+
+  	
    </div>
   </div>
  </div>
