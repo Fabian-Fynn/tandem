7b3809f9b406570fed38d50bf0ec8376047b284b
Fabian Johannes Hoffmann <fhoffmann.mmtb-2013@fh-salzburg.ac.at>
2014-05-25 12:34:06 +0200

    fix

--- matches.php
+++ matches.php
@@ -18,7 +18,7 @@
 	    $matchesString = implode(',', $matches);
 	    $matchesString = substr($matchesString, 1);
 
-		$matchedPeople = $dbh->query("Select * FROM user WHERE id IN ($matchesString)");
+		$matchedPeople = $dbh->query("Select * FROM user WHERE id IN ($matchesString) AND id != $id");
 	}
 ?>
 	<div class = "wrap">
